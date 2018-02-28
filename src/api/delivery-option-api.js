c5dac7377427faf70e24be5c47a4a2c2e3598291
ethan.nguyen <ethan.nguyens@gmail.com>
2018-02-28 17:00:19 +0000

    add delivery option api

---
+++ src/api/delivery-option-api.js
@@ -0,0 +1,12 @@
+import deliveryOptions from './mockDeliveryOption';
+
+class DeliveryOptionApi {
+  static getDeliveryOption() {
+    return new Promise((resolve, reject) => {
+      resolve(deliveryOptions);
+    })
+  }
+}
+
+export default DeliveryOptionApi;
+
